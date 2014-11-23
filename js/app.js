2b79c07342b6df7f182d30c5a25dd476edba0dc9
Anson Teng <asabluegg@gmail.com>
2014-11-24 01:28:08 +0800

    ....

--- js/app.js
+++ js/app.js
@@ -25,8 +25,8 @@ window.fbAsyncInit = function() {
             //讀取個人信息
             FB.api("/me?fields=name,picture, likes.limit(60)", function(response){
               // 把資訊插入到html裡，並顯示出來
-              $('user-name').text(response.name);
-              $('user-photo').attr("src",response.picture.data.url);
+              $('.user-name').text(response.name);
+              $('.user-photo').attr("src",response.picture.data.url);
               $('#user').removeClass('hide');
               // ---------------
               // 讀取 like 的列表，並儲存到 likes, 以及下一組資料的連結到 next
