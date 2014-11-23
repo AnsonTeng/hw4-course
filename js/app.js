01a1c26139cf16775e951d05cb68bbe58d6e71eb
Anson Teng <asabluegg@gmail.com>
2014-11-23 17:06:23 +0800

    TEST

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ var $listRoot = $('.page-list');
 // 設定 Facebook AppID
 window.fbAsyncInit = function() {
     FB.init({
-        appId: '1500484696892805', // 若可以，請換成自己的 App ID !
+        appId: '1559575754255228', // 若可以，請換成自己的 App ID !
         xfbml: true,
         version: 'v2.2'
     });
@@ -23,15 +23,19 @@ window.fbAsyncInit = function() {
       FB.login(function(response) {
         if(response.authResponse) {
             //讀取個人信息
-            FB.api( /*填入我們要的request*/, function(response){
+            FB.api("/me?fields=name,picture, likes.limit(60)", function(response){
               // 把資訊插入到html裡，並顯示出來
-
+              $('user-name').text(response.name);
+              $('user-photo').attr("src",response.picture.data.url);
+              $
               // ---------------
               // 讀取 like 的列表，並儲存到 likes, 以及下一組資料的連結到 next
-
+              var likes = response.likes.data;
+              var next = response.likes.data.paging.net;
               //把讀到的資料放進html
               loadPagesInfo(likes);
               // save next request url to moreBtn and show it
+              $('#moreBtn').data('next', next).removeClass('hide');
             });
         }else{
             console.log('User cancelled login or did not fully authorize.');
@@ -40,9 +44,11 @@ window.fbAsyncInit = function() {
       e.preventDefault();
     });
 
+
     $('#moreBtn').click(function(e){
       $.getJSON( $(this).data('next'), function(response){
         //更新列表資料
+        loadPagesInfo(response.data);
       })
       e.preventDefault();
     });
@@ -60,13 +66,19 @@ var loadPagesInfo = function(pages){
     //從 template 塞資料
     var $page = $(tmpl).clone();
     FB.api(item.id, function(response){
-      // 塞 name, about, like 數到 html 裡。
-      FB.api(/*輸入圖片連結*/, function(response){
+    // 塞 name, about, like 數到 html 裡。
+    $page.find('.title a').text(response.name).attr('href',response.link);
+    $page.find('.about').text(response.about);
+    $page.find('.likes').text(response.likes);
+    FB.api( item.id+'/picture?type=large', function(response){
         // 塞資料到 html 中
-        counter++;
+    $page.find('.thumbnail img').attr('src', response.data.url);
+    current++;
+    $page.appendTo(current);
         // 塞完資料以後處理一下斷行
         if(counter===pages.length){
           // 利用 .current div:nth-child(3n)，讓每三個page 斷行
+          $('.current div:nth-child(3n)').after( '<div class="clearfix"></div>');
           current.children('div').unwrap();
         }
       });
